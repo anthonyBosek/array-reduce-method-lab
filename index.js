f776a840a9ded35da3e29a936b0a8d4f73194215
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-14 19:53:08 -0700

    reduce so poweful

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((a, b) => a + b);
+// const subtract = batteryBatches.reduce((a, b) => a - b);
+// const mutiply = batteryBatches.reduce((a, b) => a * b);
+// const divide = batteryBatches.reduce((a, b) => a / b);
+
+// console.log(subtract);
+// console.log(mutiply);
+// console.log(divide);
